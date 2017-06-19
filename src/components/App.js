bd070fb1ccff680e45ce1da350c51d483d347178
Elizabeth <ehuntsman@wavetronix.com>
2017-06-19 14:33:29 -0600

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-
 import "./App.css";
 
 import Nav from "./Nav/Nav";
+import router from '../router';
 
 export function App( { children } ) {
 	return (
 		<div className="app">
 			<Nav />
+			{router}
 		</div>
 	);
 }
